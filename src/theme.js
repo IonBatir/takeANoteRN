bfaf9b27fc8d1bbb8339dffce6821991dff68339
Ion Batîr <ionbatir@gmail.com>
2021-06-05 16:29:54 +0300

    Add theme

---
+++ src/theme.js
@@ -0,0 +1,30 @@
+export const SPACING = {
+  EXTRA_SMALL: 4,
+  SMALL: 8,
+  MEDIUM: 16,
+  LARGE: 24,
+  EXTRA_LARGE: 32,
+};
+
+export const COLOR = {
+  WHITE: '#FFF',
+  BLACK: '#000',
+  TRANSPARENT: 'rgba(255, 255, 255, 0)',
+};
+
+export const FONT_SIZE = {
+  EXTRA_EXTRA_SMALL: 10,
+  EXTRA_SMALL: 14,
+  SMALL: 16,
+  MEDIUM: 18,
+  LARGE: 20,
+  EXTRA_LARGE: 24,
+  EXTRA_EXTRA_LARGE: 32,
+};
+
+export const DEBUG = {
+  borderWidth: 1,
+  borderColor: COLOR.RED,
+};
+
+export const LIST_ITEM_HEIGHT = 60;
