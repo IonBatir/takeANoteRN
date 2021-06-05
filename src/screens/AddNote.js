768d069bf69627d4d7b2b5808923e3c6329cc91e
Ion Batîr <ionbatir@gmail.com>
2021-06-05 15:02:43 +0300

    Add AddNote screen

---
+++ src/screens/AddNote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.centerContainer}>
+      <Text>AddNote</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  centerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
