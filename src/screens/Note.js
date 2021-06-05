e70a2868c6a32489ec8801923e47edf3c6209aef
Ion Batîr <ionbatir@gmail.com>
2021-06-05 15:03:48 +0300

    Add Note screen

---
+++ src/screens/Note.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
+
+export default function Note() {
+  return (
+    <SafeAreaView style={styles.centerContainer}>
+      <Text>Note</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  centerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
