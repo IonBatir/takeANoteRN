8e438d9903e8d35501ceb40686fadf75d6d59e8e
Ion Batîr <ionbatir@gmail.com>
2021-06-05 18:39:47 +0300

    Refactor api

--- src/api.js
+++ src/api.js
@@ -1,13 +1,15 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const getNotes = () =>
-  AsyncStorage.getAllKeys().then(notesKeys =>
-    Promise.all(notesKeys.map(noteKey => AsyncStorage.getItem(noteKey))),
-  );
+  AsyncStorage.getAllKeys()
+    .then(notesKeys => AsyncStorage.multiGet(notesKeys))
+    .then(notes =>
+      Promise.resolve(
+        notes.map(note => ({id: note[0], ...JSON.parse(note[1])})),
+      ),
+    );
 
-export const addNote = (title, content) => {
-  const key = Date.now();
-  return AsyncStorage.setItem(key, JSON.stringify({title, content}));
-};
+export const addNote = (title, content) =>
+  AsyncStorage.setItem('@' + Date.now(), JSON.stringify({title, content}));
 
-export const deleteNote = key => AsyncStorage.removeItem(key);
+export const deleteNote = key => AsyncStorage.removeItem('@' + key);
