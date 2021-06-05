d4e969c7494fcfebe8c715640d60449d9dddd9af
Ion Batîr <ionbatir@gmail.com>
2021-06-05 18:48:29 +0300

    Set Note screen

--- src/screens/Note.js
+++ src/screens/Note.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import {SafeAreaView, Text, StyleSheet} from 'react-native';
 
-export default function Note() {
+import {SPACING, FONT_SIZE} from '../theme';
+
+export default function Note({route}) {
+  const {title, content} = route.params;
+
   return (
-    <SafeAreaView style={styles.centerContainer}>
-      <Text>Note</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.content}>{content}</Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  centerContainer: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    margin: SPACING.MEDIUM,
+  },
+  title: {
+    fontFamily: 'Avenir',
+    fontSize: FONT_SIZE.EXTRA_EXTRA_LARGE,
+    fontWeight: 'bold',
+    borderBottomWidth: 1,
+    marginTop: SPACING.MEDIUM,
+  },
+  content: {
+    fontSize: FONT_SIZE.MEDIUM,
+    fontStyle: 'italic',
+    marginTop: SPACING.SMALL,
   },
 });
